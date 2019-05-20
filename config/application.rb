8662fce92fbfe35f5ccd4d6eb62a3b9130176d85
yuki.kurog <hndfra@gmail.com>
2019-05-20 17:23:39 +0900

    修正（kuroko918：こちらは要りますか？ ）

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    #...
     config.generators do |g|
       g.assets     false
       g.helper     false
